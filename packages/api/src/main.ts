57b9ca7dd9644f4e6ed2cef124ba3aa9fb5754f0
Stuart Lynn <stuart.lynn@gmail.com>
2020-09-14 15:27:17 -0400

    Adding swagger to the API server to document API

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('COVID modeling API')
+    .setDescription('An API for running COVID models')
+    .setVersion('1.0')
+    .addTag('COVID')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(4000);
 }
+
 bootstrap();
