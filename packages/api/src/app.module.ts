f22d9e162d56062e634c5bf8ac2fde4f3d655e4d
Stuart Lynn <stuart.lynn@gmail.com>
2020-09-16 17:50:33 -0400

    Stubbing out a model endpoint for predictions

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ModelController } from './model/model.controller';
+import { ModelService } from './model/model.service';
+import { CountryController } from './country/country.controller';
+import { CountryModule } from './country/country.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CountryModule, CountryModule],
+  controllers: [AppController, ModelController],
+  providers: [AppService, ModelService],
 })
 export class AppModule {}
