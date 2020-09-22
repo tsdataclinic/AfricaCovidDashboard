fd910aa4cec3a2a3c2efea2a0d810f265e43dcb3
Stuart Lynn <stuart.lynn@gmail.com>
2020-09-22 16:55:23 -0400

    Hooks for the different endpoints we have on the server

--- packages/frontend/src/hooks/useCountryStats.ts
+++ packages/frontend/src/hooks/useCountryStats.ts
@@ -1,13 +1,13 @@
 import { useQuery } from 'react-query';
 
-const getCountryStats = async (_: any, country: string) => {
+const getCountryStats = async (_: any, country: string | undefined) => {
     const request = await fetch(
-        `${process.env.SERVER_URL}/country/${country}/stats`
+        `${process.env.REACT_APP_API_LOCATION}/country/${country}/stats`
     );
     const result = await request.json();
     return result;
 };
 
-export default function useCountryStats(country: string) {
+export default function useCountryStats(country: string | undefined) {
     return useQuery(['countryStats', country], getCountryStats);
 }
