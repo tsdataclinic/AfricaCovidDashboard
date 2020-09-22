c6997ea836a342ada9f77f9a624d5f5dd193dc72
Stuart Lynn <stuart.lynn@gmail.com>
2020-09-22 13:24:06 -0400

    cleanup after rebase

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,35 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
-<<<<<<< HEAD
 import AppLayout from './components/Layout';
-=======
-import { useTranslation } from 'react-i18next';
-import LanguagePanel from './components/LanguagePanel';
-import {
-    useQueryCache,
-    QueryCache,
-    ReactQueryCacheProvider
-} from 'react-query';
+
+import { QueryCache, ReactQueryCacheProvider } from 'react-query';
 
 const queryCache = new QueryCache();
->>>>>>> 237f630... adding basic hooks for data fetching
 
 function App() {
     return (
-<<<<<<< HEAD
-        <Shell>
-            <AppLayout />
-        </Shell>
-=======
         <ReactQueryCacheProvider queryCache={queryCache}>
             <Shell>
-                <AppHeader>
-                    <p>{t('Africa COVID Dashboard')}</p>
-                    <LanguagePanel />
-                </AppHeader>
+                <AppLayout />
             </Shell>
         </ReactQueryCacheProvider>
->>>>>>> 237f630... adding basic hooks for data fetching
     );
 }
 
